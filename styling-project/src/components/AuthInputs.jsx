07e22ff1ccc218c265c1acce423e0af184692fac
Roland Gulya <mgulyaroland@gmail.com>
2025-02-01 15:29:24 +0100

    still styling with styled

--- styling-project/src/components/AuthInputs.jsx
+++ styling-project/src/components/AuthInputs.jsx
@@ -49,6 +49,21 @@ export default function AuthInputs() {
     box-shadow: 0 1px 3px 0 rgba(0, 0, 0, 0.1), 0 1px 2px 0 rgba(0, 0, 0, 0.06);
   `;
 
+  const Button = styled.button`
+    padding: 1rem 2rem;
+    font-weight: 600;
+    text-transform: uppercase;
+    border-radius: 0.25rem;
+    color: #1f2937;
+    background-color: #f0b322;
+    border-radius: 6px;
+    border: none;
+
+    $:hover {
+      background-color: #f0920e;
+    }
+  `;
+
   return (
     <div id="auth-inputs">
       <ControleContainer>
@@ -80,12 +95,7 @@ export default function AuthInputs() {
         >
           Create a new account
         </button>
-        <button
-          className="button"
-          onClick={handleLogin}
-        >
-          Sign In
-        </button>
+        <Button onClick={handleLogin}>Sign In</Button>
       </div>
     </div>
   );
