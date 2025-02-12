35e2f76690cec634205e6e5e3046a46c075297fc
Roland Gulya <mgulyaroland@gmail.com>
2025-02-12 16:12:57 +0100

    adding error handling to the project and sirting places by distance

--- http-project/src/components/AvailablePlaces.jsx
+++ http-project/src/components/AvailablePlaces.jsx
@@ -1,23 +1,44 @@
 import { useEffect, useState } from 'react';
 import Places from './Places.jsx';
-// import ErrorPage from './Error.jsx';
-// if (error) {
-//   return <ErrorPage title="An error occurred!" message={error.message} />;
-// }
+import ErrorPage from './Error.jsx';
+import { sortPlacesByDistance } from '../loc.js';
+
 export default function AvailablePlaces({ onSelectPlace }) {
   const [isFetching, setIsFetching] = useState(false);
   const [availablePlaces, setAvailablePlaces] = useState([]);
+  const [error, setError] = useState();
   useEffect(() => {
     async function fetchPlaces() {
       setIsFetching(true);
-      const response = await fetch('http://localhost:3000/places');
-      const responseData = await response.json();
-      setAvailablePlaces(responseData.places);
-      setIsFetching(false);
+      try {
+        const response = await fetch('http://localhost:3000/places');
+        const responseData = await response.json();
+        if (!response.ok) {
+          throw new Error('Failed to fetch places');
+        }
+        navigator.geolocation.getCurrentPosition((position) => {
+          const sortedPlaces = sortPlacesByDistance(
+            responseData.places,
+            position.coords.latitude,
+            position.coords.longitude
+          );
+          setAvailablePlaces(sortedPlaces);
+          setIsFetching(false);
+        });
+      } catch (error) {
+        setError({
+          message:
+            error.message || 'Could not fetch places pleasy try again later.',
+        });
+        setIsFetching(false);
+      }
     }
     fetchPlaces();
   }, []);
 
+  if (error) {
+    return <ErrorPage title="An error occurred!" message={error.message} />;
+  }
   return (
     <Places
       title="Available Places"
