8f45558bfe109f3215739b23abd8460f3b36fe80
Roland Gulya <mgulyaroland@gmail.com>
2025-02-12 14:51:35 +0100

    fetching places

--- http-project/src/components/AvailablePlaces.jsx
+++ http-project/src/components/AvailablePlaces.jsx
@@ -1,10 +1,29 @@
+import { useEffect, useState } from 'react';
 import Places from './Places.jsx';
-
+// import ErrorPage from './Error.jsx';
+// if (error) {
+//   return <ErrorPage title="An error occurred!" message={error.message} />;
+// }
 export default function AvailablePlaces({ onSelectPlace }) {
+  const [isFetching, setIsFetching] = useState(false);
+  const [availablePlaces, setAvailablePlaces] = useState([]);
+  useEffect(() => {
+    async function fetchPlaces() {
+      setIsFetching(true);
+      const response = await fetch('http://localhost:3000/places');
+      const responseData = await response.json();
+      setAvailablePlaces(responseData.places);
+      setIsFetching(false);
+    }
+    fetchPlaces();
+  }, []);
+
   return (
     <Places
       title="Available Places"
-      places={[]}
+      places={availablePlaces}
+      isLoading={isFetching}
+      loadingText="Fetching place data ..."
       fallbackText="No places available."
       onSelectPlace={onSelectPlace}
     />
